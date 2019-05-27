dcfcd989c9397375e4514c600198a50cb6d0207d
Evandro Rocha da Cunha <evandrordacunha@gmail.com>
2019-05-26 21:45:32 -0300

    V.2.0.2
    - LOCATION API FUNCIONANDO E BUSCANDO COORDENADAS A CADA 15 SEGUNDOS.
    - CORRIGIDO INSERT DE POSTOS
    - METODO BUSCAR POSTOS IMPLEMENTADO

--- app/src/main/java/com/example/kartelapp/PrincipalActivity.java
+++ app/src/main/java/com/example/kartelapp/PrincipalActivity.java
@@ -1,12 +1,18 @@
 package com.example.kartelapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
-import android.service.autofill.FillResponse;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,21 +21,42 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PrincipalActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    //CLIENTE PARA MANIPULAÇÃO DE EVENTOS
+    FusedLocationProviderClient client;
 
+    private static final String TAG = "TESTE";
+    private static ArrayList<Posto> listaPostos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +65,15 @@ public class PrincipalActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        /**
+         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+         fab.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        .setAction("Action", null).show();
+        }
         });
-
+         */
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -59,10 +86,150 @@ public class PrincipalActivity extends AppCompatActivity
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Query query = db.collection("distribuidores");
-        
+
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        /*#########AQUI COMEÇA A IMPLEMENTAÇÃO E USO DA LOCATION API #####*/
+        onResume();
+        buscaPostos();
+        imprimirPostos();
 
 
     }
+
+    /**
+     * GERENCIAMENTO DE GPS E RETORNO DE COORDENADAS DO USUÁRIO
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //VARIAVEL USADA PARA CONTROLAR O STATUS DO PLAY SERVICES NO DISPOSITIVO DO USUARIO
+        int statusCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+        //FAZENDO O DOUBLE CHECK DO STATUS DO PLAY SERVICES
+        switch (statusCode) {
+            //AUSENTE
+            case ConnectionResult.SERVICE_MISSING:
+                //REQUER ATUALIZAÇÃO
+            case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+                //DISABILITADO
+            case ConnectionResult.SERVICE_DISABLED:
+                Log.d("TESTE", "show dialog");
+                //CASO SEJA IDENTIFICADO QUE O PLAY SERVICES ESTÁ DESABILITADO, PEDE PARA HABILITAR OU MATA A APLICAÇÃO SE NÃO ATIVAR
+                GoogleApiAvailability.getInstance().getErrorDialog(this, statusCode,
+                        0, new DialogInterface.OnCancelListener() {
+                            @Override
+                            public void onCancel(DialogInterface dialog) {
+                                finish();
+                            }
+                        }).show();
+                break;
+            //OK
+            case ConnectionResult.SUCCESS:
+                Log.d("TESTE", "Google play Services está atualizado!");
+                break;
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        //ULTIMA LOCALIZAÇÃO DO USUARIO
+        client.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            Log.d("TESTE", location.getLatitude() + " " + location.getLongitude());
+                        } else {
+                            Log.d("TESTE", "Última localização não encontrada!");
+                            //METODO QUE PEDE PARA O USUARIO ATIVAR O GPS
+                            //solicitarAtivacaoGps();
+                            controleGps();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+        //MANIPULANDO COORDENADAS DE GPS
+
+        //PEGANDO UMA INSTANCIA DO LOCATION REQUEST
+        final LocationRequest locationRequest = LocationRequest.create();
+
+        //QUERO QUE O APP BUSQUE A ATUALIZAÇÃO DO USUÁRIO A CADA 15 SEGUNDOS
+        locationRequest.setInterval(15 * 1000);
+        //SE OUTRO APP ESTIVER USANDO O RECURSO COMPARTILHADO, MEU APP VAI ATUALIZAR A CADA 5 SEGUNDOS, DO CONTRÁRIO, A CADA 15 SEGUNDOS
+        locationRequest.setFastestInterval(5 * 1000);
+        //BUSCA LOCALIZAÇÃO MAIS PRECISA DO USUARIO - IMPACTO (CONSUMO DE BATERIA)
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addAllLocationRequests(Collections.singleton(locationRequest));
+
+        //CONSTROI O LOCATION REQUEST E FAZ A VERIFICAÇÃO SE EXISTE ALGUM RECURSO ATIVO CAPAZ DE INFORMAR A LOCALIZAÇÃO
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(builder.build())
+                .addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        //REDE ATIVA
+                        Log.i("TESTE", locationSettingsResponse.getLocationSettingsStates().isNetworkLocationPresent() + "");
+
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    try {
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(PrincipalActivity.this, 10);
+                    } catch (IntentSender.SendIntentException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+
+            }
+        });
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult == null) {
+                    Log.d("TESTE", "Localização indisponível!");
+                    return;
+                }
+                //PROCURAR LOCALIZAÇÃO DISPONIVEL
+                for (Location location : locationResult.getLocations()) {
+                    Log.d("TESTE", "Latitude: " + location.getLatitude() + "");
+                    Log.d("TESTE", "Longitude:" + location.getLongitude() + "");
+                }
+            }
+
+            //DISPONIBILIDADE DA LOCALIZAÇÃO DO USUARIO
+            @Override
+            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+                //LOCALIZAÇÃO ESTÁ DISPONIVEL?
+                Log.d("TESTE", locationAvailability.isLocationAvailable() + "");
+            }
+        };
+
+        client.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -111,7 +278,7 @@ public class PrincipalActivity extends AppCompatActivity
             startActivity(intent);
 
         } else if (id == R.id.nav_slideshow) {
-            Intent intent = new Intent(getApplicationContext(), PostosDenunciadosActivity.class);
+            Intent intent = new Intent(getApplicationContext(), DenunciaActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_manage) {
@@ -134,32 +301,137 @@ public class PrincipalActivity extends AppCompatActivity
     }
 
     /**
-     * MÉTODO RESPONSAVEL POR LISTAR DETERMINADOS POSTOS
+     * MÉTODO RESPONSAVEL POR LISTAR TODOS POSTOS DE COMBUSTIVEIS DO ESTADO DO RS CADASTRADOS NA BASE
+     * ALIMENTANDO UMA LISTA LOCAL EM MEMÓRIA  DISPONIBILIZADA PARA AUXILIAR OUTRAS ATIVIDADES DA APLICAÇÃO
      */
-    private void buscaPostos() {
-        //PEGA A COLEÇÃO DE DISTRIBUIDORES
-        FirebaseFirestore.getInstance().collection("/distribuidores")
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    //ESCUTANDO OS EVENTOS
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                        //VERIFICANDO SE NÃO EXISTE NENHUM ERRO
-                        if (e != null) {
-                            Log.e("Erro", e.getMessage(), e);
-                            return;
-                        }
-                        //LISTA BASEADA NA COLEÇÃO DE POSTOS
-                        List<DocumentSnapshot> sqlPostos = queryDocumentSnapshots.getDocuments();
-                        for (DocumentSnapshot doc : sqlPostos) {
+    private List buscaPostos() {
 
-                            //PARA CADA POSTO QUE EU ENCONTRAR, TRANSFORMO EM OBJETO POSTO
-                            Posto p = doc.toObject(Posto.class);
-                            if (doc.contains("487342")) {
-                                Log.d("Teste", p.getRazaoSocial());
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("postos")
+                .whereEqualTo("ESTADO", "RS")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (final QueryDocumentSnapshot document : task.getResult()) {
+                                final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                                DocumentReference docRef = db.collection("postos").document(document.getId());
+                                docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                        Posto p = documentSnapshot.toObject(Posto.class);
+                                        listaPostos.add(p);
+                                        Log.d(TAG, document.getId() + " adicionado na lista!");
+                                    }
+                                });
+
                             }
+                        } else {
+                            Log.d(TAG, "Erro ao ler dados do Firestore!", task.getException());
                         }
-                    }
-                });
+                    }});
+        return listaPostos;
+    }
+
+
+    //PEDINDO PARA O USUARIO ATIVAR O GPS
+    private void solicitarAtivacaoGps() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        Intent callGPSSettingIntent = new Intent(
+                                Settings.ACTION_LOCALE_SETTINGS);
+                        startActivity(callGPSSettingIntent);
+                        break;
+                }
+            }
+        };
+
+        android.app.AlertDialog.Builder alertDialog = new android.app.AlertDialog.Builder(this);
+        //MENSAGEM PARA O USUARIO
+        alertDialog.setMessage("Para utilizar o Kartel App você precisa permitir a ativação do GPS. Deseja ativa -lo ?");
+
+        // ACTION PARA CONFIGURAR
+        alertDialog.setPositiveButton("Configurar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        // ACTION DO BOTÃO CANCELAR
+        alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertDialog.show();
+    }
+
+    /**
+     * ALERTAS DE CONFIGRAÇÃO DE GPS
+     */
+    public void controleGps() {
+        android.app.AlertDialog.Builder alertDialog = new android.app.AlertDialog.Builder(this);
+        alertDialog.setTitle("GPS");
+
+        //MENSAGEM PARA O USUARIO
+        alertDialog.setMessage("Para utilizar o Kartel App você precisa permitir a ativação do GPS. Deseja ativa -lo ?");
+
+        // ACTION PARA CONFIGURAR
+        alertDialog.setPositiveButton("Configurar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        // ACTION DO BOTÃO CANCELAR
+        alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertDialog.show();
+    }
+
+    public boolean buscarLocalizacao(Context context) {
+        int REQUEST_PERMISSION_LOCALIZATION = 221;
+        boolean res = true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+
+                res = false;
+                ActivityCompat.requestPermissions((PrincipalActivity) context, new String[]{
+                                Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_PERMISSION_LOCALIZATION);
+
+            }
+        }
+        return res;
+    }
+
+
+    private String imprimirPostos() {
+        String s = "";
+        for (int i = 0; i < listaPostos.size(); i++) {
+            s = s + listaPostos.get(i).toString() + "\n";
+            Log.d("Posto:  ", listaPostos.get(i).getRazaoSocial()+" está na lista!");
+
+        }
+        return "Postos cadastrados  " + s;
     }
 
 }
